5a5ad3d12cacd1dc87f47f7c27fd3fcb7c6f9ca9
icheng <yicheng-me@qq.com>
2019-10-20 11:56:59 +0800

    chore: styles

--- src/pages/piano/index/comp/panels/visit.vue
+++ src/pages/piano/index/comp/panels/visit.vue
@@ -76,71 +76,71 @@ export default {
 
 <style lang="stylus">
 .visit-ct {
-    >.logo-ct {
-        position relative
-        margin 0px 0 0;
-        >.pn-logo {
-            position absolute;
-            top 0px;
-            left 18px;
-            width 40px;
-            max-width 30%;
-            overflow hidden;
-            border-radius 110px;
-            border 2px solid #fff;
-            box-shadow 0 0 10px #0003;
-            margin 0 auto;
-            &:before {
-                padding-top 100%;
-                content '';
-                display block;
-            }
-            >img{
-                position absolute;
-                top 0;
-                max-height 100%;
-            }
-        }
-        >.pn-info {
-            margin 0 0 0 78px;
-            >h1{
-                padding 4px 0 0 0;
-                margin 0;
-                font-size 16px;
-                font-weight bold;
-            }
-            >p {
-                margin 0;
-                word-break break-all;
-                // margin-top 15px;
-            }
-        }
+  padding: 1em 0 0 0;
+  >.logo-ct {
+    position relative
+    margin 0px 0 0;
+    >.pn-logo {
+      position absolute;
+      top 0px;
+      left 18px;
+      width 40px;
+      max-width 30%;
+      overflow hidden;
+      border-radius 110px;
+      border 2px solid #fff;
+      box-shadow 0 0 10px #0003;
+      margin 0 auto;
+      &:before {
+        padding-top 100%;
+        content '';
+        display block;
+      }
+      >img{
+        position absolute;
+        top 0;
+        max-height 100%;
+      }
+    }
+    >.pn-info {
+      margin 0 0 0 78px;
+      >h1{
+        padding 4px 0 0 0;
+        margin 0;
+        font-size 16px;
+        font-weight bold;
+      }
+      >p {
+        margin 0;
+        word-break break-all;
+        // margin-top 15px;
+      }
+    }
+  }
+  >.tips-wrapper {
+    padding 50px 0 30px 0;
+    margin 0 20px;
+    font-size 17px;
+    >div {
+      text-align center;
+      transition opacity 0.3s;
+      &.hidden {
+        opacity 0;
+      }
+      b {
+        margin 0 6px;
+        font-size 18px;
+        color #444;
+      }
+    }
+  }
 
+  >.doc-div{
+    margin 0 15px;
+    padding 10px 0 0 0;
+    p {
+      margin 8px 0;
     }
-    >.tips-wrapper {
-        padding 50px 0 30px 0;
-        margin 0 20px;
-        font-size 17px;
-        >div {
-            text-align center;
-            transition opacity 0.3s;
-            &.hidden {
-                opacity 0;
-            }
-            b {
-                margin 0 6px;
-                font-size 18px;
-                color #444;
-            }
-        }
-    }
-
-    >.doc-div{
-        margin 0 15px;
-        padding 10px 0 0 0;
-        p {
-            margin 8px 0;
-        }
-    }
+  }
 }
 </style>
