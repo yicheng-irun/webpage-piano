e9934c4f348a36beaa5c8aabec34ed8ab65355ea
icheng <yicheng-me@qq.com>
2019-10-20 14:43:33 +0800

    feat: 修改markdown，加上打赏

---
+++ src/comp/xw-comp/dashang/index.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="dashang">
+        <div class="d-title">
+            {{ title }}
+        </div>
+        <div class="item">
+            <img
+                src="./ds-weixin.png"
+                alt=""
+            >
+            <div class="ds-title">
+                微信
+            </div>
+        </div>
+        <div class="item">
+            <img
+                src="./ds-alipay.png"
+                alt=""
+            >
+            <div class="ds-title">
+                支付宝
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '谢谢钢琴家们的打赏~ 助我早日脱离工厂流水线',
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="stylus">
+.dashang {
+    display inline-block;
+    text-align center;
+    background #0001;
+    >.d-title {
+        text-align center;
+        font-size 16px;
+        line-height 2
+        padding 6px 0 6px 0;
+        border-top 3px solid #f005;
+    }
+    >.item {
+        max-width 168px;
+        display inline-block;
+        vertical-align top;
+        margin 0 1em;
+        >.ds-title {
+            text-align center;
+            font-size 18px;
+            padding 0.2em 0 1em;
+            text-shadow 0 0 2px;
+        }
+        >img {
+            width 100%
+            border 1px solid #fff
+        }
+    }
+}
+</style>
--- src/pages/index/app.vue
+++ src/pages/index/app.vue
@@ -1,22 +1,40 @@
 <template>
-  <div class="hello">
+  <div class="home-page">
     <pianoBanner />
+    <div class="dashang-wrapper">
+      <p class="intros">
+        公仔厂的流水线上有点忙，平常工作的话基本上每天加班要晚上9点才下班，每个周末就抽出一些时间陆陆续续开发了一年，应该写了上万行代码。
+        后面争取多抽一些时间，把想加的功能统统都加上去。
+      </p>
+      <dashang />
+    </div>
   </div>
 </template>
 
 <script>
 import pianoBanner from './comps/piano-banner.vue';
+import dashang from '../../comp/xw-comp/dashang/index.vue';
 
 export default {
   name: 'HelloWorld',
   components: {
     pianoBanner,
+    dashang,
   },
   props: {
   },
 };
 </script>
 
-<style>
-
+<style lang="stylus">
+.home-page {
+  >.dashang-wrapper {
+    margin 6em 0 3em;
+    text-align center;
+    >.intros {
+      font-size 14px;
+      margin 1em 1em;
+    }
+  }
+}
 </style>
